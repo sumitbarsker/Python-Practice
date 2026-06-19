e2757db418cacf0447ccc91372df93b31947b32d
sumit barsker <sumitbarsker876@gmail.com>
2026-06-19 22:33:10 +0530

    Add script to find the largest of three numbers

---
+++ day8.py
@@ -0,0 +1,10 @@
+a = 10
+b = 25
+c = 15
+
+if a >= b and a >= c:
+    print("Largest number is", a)
+elif b >= a and b >= c:
+    print("Largest number is", b)
+else:
+    print("Largest number is", c)
