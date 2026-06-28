3989eb29146c4f91ea15450e77f7e6a9cc120cbe
sumit barsker <sumitbarsker876@gmail.com>
2026-06-28 23:15:10 +0530

    Add prime number check for a given number

--- day13.py
+++ day13.py
@@ -1,1 +1,11 @@
+num = 17
 
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print(num, "is not a Prime Number")
+            break
+    else:
+        print(num, "is a Prime Number")
+else:
+    print(num, "is not a Prime Number")
