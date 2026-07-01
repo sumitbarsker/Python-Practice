5424f0f101a64bc2058f5814955b281ccf9d1c25
sumit barsker <sumitbarsker876@gmail.com>
2026-07-01 14:28:00 +0530

    Add Armstrong number check in day15.py

--- day15.py
+++ day15.py
@@ -1,1 +1,14 @@
+num = 153
 
+temp = num
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+
+if num == sum:
+    print(num, "is an Armstrong Number")
+else:
+    print(num, "is not an Armstrong Number")
