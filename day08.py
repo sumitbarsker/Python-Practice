8c046ccae5bf966f51369d2ec12ed5bb04d093a1
sumit barsker <sumitbarsker876@gmail.com>
2026-08-26 21:00:02 +0530

    Fix duplicate print statement for largest number

--- day08.py
+++ day08.py
@@ -5,6 +5,6 @@ c = 15
 if a >= b and a >= c:
     print("Largest number is", a)
 elif b >= a and b >= c:
-    print("Largest number is", b) 
+    print("Largest number is", b)
 else:
     print("Largest number is", c)
