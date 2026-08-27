fd57cd87e178e165df10672d0d072f7ce079249d
sumit barsker <sumitbarsker876@gmail.com>
2026-08-27 17:15:22 +0530

    Count word frequency in a given text

--- day26.py
+++ day26.py
@@ -1,1 +1,15 @@
+text = "python is easy and python is powerful"
 
+words = text.split()
+
+frequency = {}
+
+for word in words:
+    if word in frequency:
+        frequency[word] += 1
+    else:
+        frequency[word] = 1
+
+print("Word Frequency:")
+for word, count in frequency.items():
+    print(word, ":", count)
