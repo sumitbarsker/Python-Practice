3f925952e1eef3488f5359ef2c1d0119b12c3853
sumit barsker <sumitbarsker876@gmail.com>
2026-08-27 19:14:05 +0530

    Add marks dictionary and calculate total and average

--- day27.py
+++ day27.py
@@ -1,1 +1,16 @@
+marks = {
+    "Maths": 85,
+    "Python": 90,
+    "English": 78,
+    "AI": 88
+}
 
+total = 0
+
+for subject, mark in marks.items():
+    total += mark
+
+average = total / len(marks)
+
+print("Total Marks:", total)
+print("Average Marks:", average)
