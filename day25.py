2a71a723264f994756722b3005de654173e4ea8f
sumit barsker <sumitbarsker876@gmail.com>
2026-08-27 17:07:32 +0530

    Find most frequent number in a list

--- day25.py
+++ day25.py
@@ -1,1 +1,18 @@
+numbers = [10, 20, 10, 30, 20, 10, 40, 20, 20]
 
+most_frequent = numbers[0]
+max_count = 0
+
+for num in numbers:
+    count = 0
+
+    for item in numbers:
+        if item == num:
+            count += 1
+
+    if count > max_count:
+        max_count = count
+        most_frequent = num
+
+print("Most frequent number:", most_frequent)
+print("Frequency:", max_count)
