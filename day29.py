2707e565c3adacd17b3bc2e8b4589f3e7d77a9ec
sumit barsker <sumitbarsker876@gmail.com>
2026-08-27 19:22:48 +0530

    Add function to check if a number is even or odd

--- day29.py
+++ day29.py
@@ -1,1 +1,12 @@
+def check_even_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
 
+
+number = 25
+
+result = check_even_odd(number)
+
+print(number, "is", result)
