b579bf7a111998f276dcf3f9d6653192b1810023
sumit barsker <sumitbarsker876@gmail.com>
2026-08-27 22:01:12 +0530

    Update day30.py

--- day30.py
+++ day30.py
@@ -1,1 +1,25 @@
+def add(a, b):
+    return a + b
 
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+
+a = 20
+b = 5
+
+print("Addition:", add(a, b))
+print("Subtraction:", subtract(a, b))
+print("Multiplication:", multiply(a, b))
+print("Division:", divide(a, b))
