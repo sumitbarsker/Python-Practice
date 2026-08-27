204ad74c5ac592a89729f14b655c62cd8156895c
sumit barsker <sumitbarsker876@gmail.com>
2026-08-27 17:00:13 +0530

    Implement bubble sort algorithm for sorting numbers

--- day23.py
+++ day23.py
@@ -1,1 +1,8 @@
+numbers = [64, 25, 12, 22, 11]
 
+for i in range(len(numbers)):
+    for j in range(i + 1, len(numbers)):
+        if numbers[i] > numbers[j]:
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+
+print("Sorted List:", numbers)
