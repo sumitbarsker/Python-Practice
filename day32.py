c9fdbe1a6912e9207bac477e63e74d20b8cf8c27
sumit barsker <sumitbarsker876@gmail.com>
2026-08-30 11:51:48 +0530

    Count uppercase and lowercase letters in a string

--- day32.py
+++ day32.py
@@ -1,1 +1,13 @@
+text = "Python Programming Is Fun"
 
+uppercase = 0
+lowercase = 0
+
+for char in text:
+    if char.isupper():
+        uppercase += 1
+    elif char.islower():
+        lowercase += 1
+
+print("Uppercase letters:", uppercase)
+print("Lowercase letters:", lowercase)
