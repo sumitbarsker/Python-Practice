484a789c410d123788e328eba26ce93261b83ec8
sumit barsker <sumitbarsker876@gmail.com>
2026-09-05 20:45:39 +0530

    Update day36.py

--- day36.py
+++ day36.py
@@ -1,1 +1,13 @@
+text = "Python Programming"
 
+vowels = 0
+consonants = 0
+
+for char in text.lower():
+    if char in "aeiou":
+        vowels += 1
+    elif char.isalpha():
+        consonants += 1
+
+print("Vowels:", vowels)
+print("Consonants:", consonants)
