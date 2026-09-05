b2ebe82d09081e43433cfc8948fe42ae99392324
sumit barsker <sumitbarsker876@gmail.com>
2026-09-05 20:38:46 +0530

    Count letters, digits, and special characters in text

--- day33.py
+++ day33.py
@@ -1,1 +1,17 @@
+text = "Python123@Hello!"
 
+letters = 0
+digits = 0
+special = 0
+
+for char in text:
+    if char.isalpha():
+        letters += 1
+    elif char.isdigit():
+        digits += 1
+    else:
+        special += 1
+
+print("Letters:", letters)
+print("Digits:", digits)
+print("Special characters:", special)
