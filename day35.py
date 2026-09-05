814b9b8bb9e97205a697e7f781dbd81e96d02e34
sumit barsker <sumitbarsker876@gmail.com>
2026-09-05 20:44:16 +0530

    Add palindrome check for a given string

--- day35.py
+++ day35.py
@@ -1,1 +1,11 @@
+text = "madam"
 
+reversed_text = ""
+
+for char in text:
+    reversed_text = char + reversed_text
+
+if text == reversed_text:
+    print("The string is a palindrome")
+else:
+    print("The string is not a palindrome")
