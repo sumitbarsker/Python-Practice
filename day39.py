217a2b59ca6f199a68cb43ae33022a6211ca658f
sumit barsker <sumitbarsker876@gmail.com>
2026-09-12 20:25:24 +0530

    Find and print duplicate characters in a string

--- day39.py
+++ day39.py
@@ -1,1 +1,9 @@
+text = "programming"
 
+duplicates = []
+
+for char in text:
+    if text.count(char) > 1 and char not in duplicates:
+        duplicates.append(char)
+
+print("Duplicate characters:", duplicates)
