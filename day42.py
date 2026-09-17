6e77f9024a8225a150b06ee4a7d76688e64496ce
sumit barsker <sumitbarsker876@gmail.com>
2026-09-17 19:06:17 +0530

    Find and print duplicate elements in a list

--- day42.py
+++ day42.py
@@ -1,1 +1,9 @@
+numbers = [10, 20, 10, 30, 40, 20, 50, 10]
 
+duplicates = []
+
+for num in numbers:
+    if numbers.count(num) > 1 and num not in duplicates:
+        duplicates.append(num)
+
+print("Duplicate elements:", duplicates)
