b7db1436f7202dbac2808984e7000df946ec039b
sumit barsker <sumitbarsker876@gmail.com>
2026-09-17 19:04:47 +0530

    Find largest and second largest numbers in list

--- day41.py
+++ day41.py
@@ -1,1 +1,14 @@
+numbers = [12, 45, 7, 89, 34, 56]
 
+largest = numbers[0]
+second_largest = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        second_largest = largest
+        largest = num
+    elif num > second_largest and num != largest:
+        second_largest = num
+
+print("Largest:", largest)
+print("Second Largest:", second_largest)
