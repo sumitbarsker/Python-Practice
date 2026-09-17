57caf15aa54deaefb44d1e77e2b4025fb80713c1
sumit barsker <sumitbarsker876@gmail.com>
2026-09-17 19:19:46 +0530

    Find common elements between two lists

--- day43.py
+++ day43.py
@@ -1,1 +1,10 @@
+list1 = [10, 20, 30, 40, 50]
+list2 = [30, 40, 50, 60, 70]
 
+common = []
+
+for num in list1:
+    if num in list2 and num not in common:
+        common.append(num)
+
+print("Common elements:", common)
