028f86786d334379ee02a5d1ed300ef50d483100
sumit barsker <sumitbarsker876@gmail.com>
2026-09-17 19:21:12 +0530

    Merge two lists into a combined list

--- day44.py
+++ day44.py
@@ -1,1 +1,14 @@
+list1 = [10, 20, 30, 40]
+list2 = [30, 40, 50, 60]
 
+combined = []
+
+for num in list1:
+    if num not in combined:
+        combined.append(num)
+
+for num in list2:
+    if num not in combined:
+        combined.append(num)
+
+print("Combined list:", combined)
