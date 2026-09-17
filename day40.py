5efbc56bb611ebf477e01eecdf3af547182ab50c
sumit barsker <sumitbarsker876@gmail.com>
2026-09-17 19:02:59 +0530

    Update day40.py

--- day40.py
+++ day40.py
@@ -1,1 +1,10 @@
+text = "programming"
 
+result = ""
+
+for char in text:
+    if char not in result:
+        result += char
+
+print("Original string:", text)
+print("String without duplicates:", result)
