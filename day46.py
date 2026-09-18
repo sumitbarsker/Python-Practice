9436b49699e2822494f6ea8a23272774d28c9d2d
sumit barsker <sumitbarsker876@gmail.com>
2026-09-18 22:19:59 +0530

    Find missing number in a sequence

--- day46.py
+++ day46.py
@@ -1,1 +1,14 @@
+numbers = [1, 2, 3, 5, 6, 7]
 
+n = 7
+
+expected_sum = n * (n + 1) // 2
+
+actual_sum = 0
+
+for num in numbers:
+    actual_sum += num
+
+missing_number = expected_sum - actual_sum
+
+print("Missing number:", missing_number)
