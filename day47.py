a9d7bc1952659cf4c701fd6e1a83f5557a44a164
sumit barsker <sumitbarsker876@gmail.com>
2026-09-19 20:13:20 +0530

    Find smallest and largest numbers in a list

--- day47.py
+++ day47.py
@@ -1,1 +1,17 @@
+numbers = [10, 25, 7, 45, 30, 15]
 
+smallest = numbers[0]
+largest = numbers[0]
+
+for num in numbers:
+    if num < smallest:
+        smallest = num
+
+    if num > largest:
+        largest = num
+
+difference = largest - smallest
+
+print("Smallest number:", smallest)
+print("Largest number:", largest)
+print("Maximum difference:", difference)
