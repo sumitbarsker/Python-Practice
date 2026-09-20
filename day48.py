4a672636c11bfadd1c1559f50d175d54363c2462
sumit barsker <sumitbarsker876@gmail.com>
2026-09-20 13:16:51 +0530

    Count positive, negative, and zero values in list

--- day48.py
+++ day48.py
@@ -1,1 +1,17 @@
+numbers = [10, -5, 0, 25, -10, 0, 15, -3]
 
+positive = 0
+negative = 0
+zero = 0
+
+for num in numbers:
+    if num > 0:
+        positive += 1
+    elif num < 0:
+        negative += 1
+    else:
+        zero += 1
+
+print("Positive numbers:", positive)
+print("Negative numbers:", negative)
+print("Zeros:", zero)
