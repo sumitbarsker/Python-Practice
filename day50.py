afa7bc7893d9249808691e3d34d1e25c42c58294
sumit barsker <sumitbarsker876@gmail.com>
2026-09-22 19:36:20 +0530

    Calculate total, average, highest, and lowest marks

--- day50.py
+++ day50.py
@@ -1,1 +1,21 @@
+marks = [78, 85, 92, 67, 88, 74, 95]
 
+total = 0
+highest = marks[0]
+lowest = marks[0]
+
+for mark in marks:
+    total += mark
+
+    if mark > highest:
+        highest = mark
+
+    if mark < lowest:
+        lowest = mark
+
+average = total / len(marks)
+
+print("Total Marks:", total)
+print("Average Marks:", average)
+print("Highest Marks:", highest)
+print("Lowest Marks:", lowest)
